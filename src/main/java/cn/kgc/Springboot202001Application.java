3e0b708ab61e94c25badeb64bbb759ef4abb8dfc
xuruifeng <xuruifenglg@126.com>
2021-01-19 16:25:30 +0800

    111111

---
+++ src/main/java/cn/kgc/Springboot202001Application.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot202001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot202001Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/kgc/Springboot202001ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot202001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
